9e8d5228e4fa90195d6c7603feb8ad5c9389eb08
Mohammad <khalisrasuli@gmai.com>
2022-03-07 23:23:11 +0000

    the full version of the code is uploaded

--- Employee_Data_Entry.py
+++ Employee_Data_Entry.py
@@ -9,3 +9,13 @@ for index in range(Number_of_employees):
     Employee_last_name = input("Please state your last name: ")
     while len(Employee_last_name) == 0:
         print("Invalid data, please enter again: ")
+    Employee_age = int(input("Please state your age: "))
+    age = int(Employee_age)
+    while (age < 18) or (age > 100):
+        print("Invalid data, please enter again")
+
+    print(f"Employees name is {Employee_first_name} {Employee_last_name} and their age is {Employee_age}.")
+Age_sum = 0
+Age_sum = Age_sum + age
+if Age_sum > 500:
+    print("Warning! Age Sum is over 500")
